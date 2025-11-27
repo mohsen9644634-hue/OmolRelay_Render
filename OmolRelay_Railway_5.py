a47191596940a1994cebbecef115c52eb9913e6c
mohsen9644634@gmail.com <mohsen9644634@gmail.com>
2025-11-28 00:32:43 +0330

    Update OmolRelay_Railway_5.py

--- OmolRelay_Railway_5.py
+++ OmolRelay_Railway_5.py
@@ -117,4 +117,6 @@ async def home():
 # ---------------------------
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
