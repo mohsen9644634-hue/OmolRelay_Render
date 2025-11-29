affb99d995f6659dc5c77dfd0bb20df2848f9549
mohsen9644634@gmail.com <mohsen9644634@gmail.com>
2025-11-29 16:59:25 +0330

    Update parnya.py

--- parnya.py
+++ parnya.py
@@ -1,14 +1,12 @@
-    import os
-    from flask import Flask
+import os
+from flask import Flask
 
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    @app.route('/')
-    def hello_world():
-        return 'سلام پویا! پروژه شما با موفقیت در Render.com مستقر شد.'
+@app.route('/')
+def hello_world():
+    return 'سلام پویا! پروژه شما با موفقیت در Render.com مستقر شد.'
 
-    if __name__ == '__main__':
-        port = int(os.environ.get('PORT', 5000))
-        app.run(host='0.0.0.0', port=port)
-
-
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
