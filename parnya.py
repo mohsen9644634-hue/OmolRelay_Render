1185c8421a5b0fbfea0337bee3f47a5049d2ecbe
mohsen9644634@gmail.com <mohsen9644634@gmail.com>
2025-11-29 16:06:55 +0330

    Create parnya.py

---
+++ parnya.py
@@ -0,0 +1,10 @@
+here        from flask import Flask
+
+        app = Flask(__name__)
+
+        @app.route('/')
+        def hello_world():
+            return 'سلام پویا! پروژه شما با موفقیت در Render.com مستقر شد.'
+
+        if __name__ == '__main__':
+            app.run(debug=True)
