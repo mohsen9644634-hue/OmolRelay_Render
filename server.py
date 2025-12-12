62965a96342faf697d9d12be665612dd504e8ca0
mohsen9644634@gmail.com <mohsen9644634@gmail.com>
2025-12-12 13:21:46 +0330

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,1 @@
-from parnya import app, start_bot, set_leverage
-import threading
-
-if __name__ == "__main__":
-    set_leverage()
-    
-    # Run trading bot in a background thread that survives Flask reloads
-    t = threading.Thread(target=start_bot, daemon=True)
-    t.start()
-
-    app.run(host="0.0.0.0", port=5000, use_reloader=False)
+from parnya import app
